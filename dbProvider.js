413b0f0fe394c4517ee1b19df611823d60e0c121
21120177 <minhhuy14012003@gmail.com>
2023-10-29 00:49:15 +0700

    Hoan thanh xu ly request cho type detail va get

--- dbProvider.js
+++ dbProvider.js
@@ -14,12 +14,9 @@ export default {
             return new Promise((resolve, reject) => {
             
             console.log(queryInfo);
-            // Parse the queryInfo string
             const str = queryInfo.toLowerCase().split('/');
             const typeInfo=str[0];
-            console.log('Type '+typeInfo);
             const classInfo=str[1];
-            console.log('ClassInfo: '+classInfo);
             let pattern='';
             let perpage;
             let page;
@@ -28,36 +25,24 @@ export default {
                 const rest=str[2].split('?');
                 pattern=rest[0];
                 const params=rest[1];
-                console.log('Parram '+params);
                 
                 const paramValues=params.split('&')
                 const firstParam=paramValues[0];
-                console.log(firstParam);
-    
                 const secondParam=paramValues[1];
-                console.log(secondParam);
+
                 perpage=firstParam.split('=')[1];
                 page=secondParam.split('=')[1];
-                console.log('Page: '+page);
-                console.log('Per Page: '+perpage);    
             }
             else {
                 pattern=str[2];
-                console.log('Pattern '+pattern);
-                console.log(typeInfo);
-                console.log(classInfo);
             }
 
             let objResult={
             };
-            console.log('ABC');
             if (typeInfo=='search')
             {
-                console.log(typeInfo);
-
                 objResult = {
-                    type: typeInfo,
-                    class: classInfo,
+                    [typeInfo]:pattern,
                     page: parseInt(page),
                     per_page: parseInt(perpage),
                     total_page: 0,
@@ -65,71 +50,141 @@ export default {
                     items:[]
                 };
 
-                console.log(classInfo);
-                if (classInfo=='movie')
-                {
-                    console.log('Type Info: '+typeInfo);
-                    for (let i=0; i<Movies.Movies.length; i++)
+                try {
+                    if (classInfo=='movie')
                     {
-                        let movie=Movies.Movies[i];
-                        if (movie.fullTitle.toLowerCase().search(pattern)!=-1)
+                        let found = false;
+                        for (let i=0; i<Movies.Movies.length; i++)
                         {
-                            objResult.items.push(movie);
-                            objResult.total+=1;
-                        }
-                    }
-                    console.log(objResult);
-                } else if (classInfo=='name')
-                {
-                    console.log('Type Info: '+typeInfo);
-                    for (let i=0; i<Movies.Movies.length; i++)
-                    {
-                        let movie=Movies.Movies[i];
-                        for (let j=0; j<movie.actorList.length; j++)
-                        {
-                            if (movie.actorList[j].name.toLowerCase().search(pattern)!=-1)
+                            let movie=Movies.Movies[i];
+                            if (movie.fullTitle.toLowerCase().search(pattern)!=-1)
                             {
                                 objResult.items.push(movie);
                                 objResult.total+=1;
-                            }
-
+                                found=true;
+                            } 
                         }
-                        
-                    }
+                        if (!found)
+                        {
+                                reject(new Error('Movie not found'));
+                        }
+                    } 
+                    else if (classInfo=='name')
+                    {
+                        let found=false;
+                        for (let i=0; i<Movies.Movies.length; i++)
+                        {
+                            let movie=Movies.Movies[i];
+                            for (let j=0; j<movie.actorList.length; j++)
+                            {
+                                if (movie.actorList[j].name.toLowerCase().search(pattern)!=-1)
+                                {
+                                    objResult.items.push(movie);
+                                    objResult.total+=1;
+                                    found=true;
+                                }
 
+                            }
+                            if (!found)
+                            {
+                                reject(new Error('Actor not found'));
+                            }
+                            
+                        }
+                    }
+                    objResult.total_page =Math.floor( objResult.total / objResult.per_page) + ((objResult.total % objResult.per_page) == 0 ? 0 : 1);
                     console.log(objResult);
+                    resolve(objResult); 
+                } catch (error) {
+                    reject(error); 
                 }
             }
 
-        
+            if (typeInfo=='detail'){
+                objResult = {
+                    id:pattern,
+                    item:{},
+                };
+                try {
+                    if (classInfo=='movie')
+                    {
+                    for (let i=0; i<Movies.Movies.length; i++)
+                    {
+                        let movie=Movies.Movies[i];
+                        if (movie.id.toLowerCase().search(pattern)!=-1)
+                        {
+                           objResult.item=movie;
+                        }
+                        
+                    }
+                     }
+                    if (classInfo=='name')
+                    {
+                        for (let i=0; i<Movies.Names.length; i++)
+                     {
+                        let actor=Movies.Names[i];
+                        if (actor.id.toLowerCase().search(pattern)!=-1)
+                        {
+                           objResult.item=actor;
+                        }
+                        
+                    }
+                    }
+                    console.log(objResult);
+                    resolve(objResult); 
+                } catch (error) {
+                    reject(error);
+                }
+                
+            }
 
-            
-            // const params = new URLSearchParams(rest.join('/').split('?')[1]);
-        
-            // Handle the query based on the type and class
-            // if (type === 'search' && className === 'movie') {
-            //     // Perform a search in the Movies module
-            //     const results = Movies.search(pattern, params);
-            //     resolve(results);
-            // } else if (type === 'detail' && className === 'movie') {
-            //     // Get the details of a specific movie in the Movies module
-            //     const details = Movies.getDetails(pattern);
-            //     resolve(details);
-            // } else if (type === 'get' && className === 'top50') {
-            //     // Get the top 50 movies in the Movies module
-            //     const top50 = Movies.getTop50();
-            //     resolve(top50);
-            // } else {
-            //     // If the query is not recognized, reject the promise
-            //     reject(new Error('Invalid query'));
-            // }
-            // });
+            if (typeInfo=='get'){
+                objResult={
+                [typeInfo]:classInfo,
+                page: parseInt(page),
+                per_page: parseInt(perpage),
+                total_page: 0,
+                total: 0,
+                items:[]
+                }
+                try {
+                    if (classInfo=='top50')
+                    {
+                    for (let i=0; i<Movies.Top50Movies.length; i++)
+                    {
+                        let movie=Movies.Top50Movies[i];
+                        objResult.items.push(movie);  
+                        objResult.total += 1; 
+                    }
+    
+                    }
+                    else if (classInfo=='mostpopular')
+                    {
+                    for (let i=0; i<Movies.MostPopularMovies.length; i++)
+                    {
+                        let movie=Movies.MostPopularMovies[i];
+                        objResult.items.push(movie);  
+                        objResult.total += 1; 
+                        
+                    }
+                    }
+                    else if (classInfo=='topboxoffice'){
+
+                    }
+                    objResult.total_page =Math.floor( objResult.total / objResult.per_page) + ((objResult.total % objResult.per_page) == 0 ? 0 : 1);
+                    console.log(objResult);
+                    resolve(objResult); // Resolve the promise with the result
+                } catch (error) {
+                    reject(error); // Reject the promise with the error
+                }
+                
+            }
             })
         },
 
         showData: function() {
             console.log(Movies);
-            this.fetch('search/name/smith?per_page=10&page=2');
+            this.fetch('detail/movie/tt0015864');
         }
     },
 
