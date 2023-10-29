57ac5d1777af58a678ccff4562ca4f003e6b51af
21120177 <minhhuy14012003@gmail.com>
2023-10-29 11:11:40 +0700

    Hoan thanh hien thi top 15 popular movies

--- Main.js
+++ Main.js
@@ -59,20 +59,19 @@ export default{
   </div>
   <div class="popular-movies">
   <h5>Most Popular</h5>
+  <div id="carouselExampleIndicators2" class="carousel slide" style="height: 40%;">
     <div class="carousel-indicators">
-      <button v-for="(movie, index) in 5" :key="index" type="button" data-bs-target="#carouselExampleCaptions" 
+      <button v-for="(movie, index) in 5" :key="index" type="button" data-bs-target="#carouselExampleIndicators2" 
     :data-bs-slide-to="index" :class="{ active: index === 0 }" :aria-label="'Movie ' + (index + 1)" 
     :aria-current="index === 0"></button>
     </div>
     <div class="carousel-inner">
     <div v-for="(group, index) in chunk(top15PopularMovies, 3)" :key="index" class="carousel-item" :class="{ active: index === 0 }">
-    <div class="d-flex">
-        <div v-for="movie in group" class="p-2 flex-fill ">
+      <div class="d-flex justify-content-around">
+        <div v-for="movie in group" class="p-2 flex-fill">
             <img :src="movie.image" class="d-block w-50 popular-movies-styles" alt="...">
         </div>
-    </div>
-</div>
-    </div>
+      </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -82,9 +81,12 @@ export default{
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
+    </div>
+    </div>
+    
   </div>
   </div>
-  <div class="popular-movies">
+  <div class="top-rating-movies">
   <h5>Top Rating</h5>
   <div id="carouselExampleIndicators3" class="carousel slide" style="height: 40%;">
   <div class="carousel-indicators indicators">
